fb92c502403d4c219c9bf0bb3f2b2f96e76bc20d
Debashish Majumdar <majumdardebashish@ymail.com>
2022-06-30 01:06:12 +0530

    commit

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,49 +4,42 @@ using System.Globalization;
 using System.IO.Pipes;
 using UnityEngine;
 using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 public class movement : MonoBehaviour
 {
-   [SerializeField]private float _speed = 7f;
-    [SerializeField]private float _mouseSensitivity = 50f;
-    [SerializeField]private float _minCameraview = -70f, _maxCameraview = 80f;
+    [SerializeField] private float _speed = 2f;
     private CharacterController _charController;
     [SerializeField] private Camera _fppcamera;
-        //[SerializeField] private Camera _tppcamera;
-        private Camera _camera =null;
-        bool istpp;
-          //[SerializeField] private GameObject _tppc;
-                    [SerializeField] private GameObject _fppc;
+    //[SerializeField] private Camera _tppcamera;
+    private Camera _camera = null;
+    bool istpp;
+    //[SerializeField] private GameObject _tppc;
+    //[SerializeField] private GameObject _fppc;
     public DialogueManager dm;
 
-    public bool canmove;
-    private int n;
-    private bool iswaving;
-    private float xRotation = 0f;
-         Vector3 moveVector;
-         private bool iswalking;
- private Animator animator;
-  float vertical,horizontal;
+    //public bool canmove;
+    //private int n;
+    //private bool iswaving;
+    //private float xRotation = 0f;
+    Vector3 moveVector;
+    //private bool iswalking;
+    //private Animator animator;
+    float vertical, horizontal;
 
     // Start is called before the first frame update
     void Start()
-    {       canmove=true;
+    {       //canmove=true;
         _charController = GetComponent<CharacterController>();
-        _fppc.SetActive(true);
-                //_tppc.SetActive(false);
+        //_fppc.SetActive(true);
+        //_tppc.SetActive(false);
 
-       _camera = _fppcamera;
+        _camera = _fppcamera;
 
-
-       istpp=false;
-
-        if(_charController == null)
-        Debug.Log("No Character Controller Attached to Player");
 
         Cursor.lockState = CursorLockMode.Locked;
-        animator= GetComponent<Animator>();
-        iswalking=false;
     }
-    IEnumerator waiter()
+    /*IEnumerator waiter()
 {       canmove=false;
      animator.SetBool("iswaving",true);
     yield return new WaitForSeconds(2);
@@ -55,25 +48,26 @@ public class movement : MonoBehaviour
 
            canmove=true;
 
-}
+}*/
 
     // Update is called once per frame
     void Update()
     {
         //Get WASD Input for Player
-       // _camera.SetActive(true);
-       if(canmove){
-             vertical = Input.GetAxis("Vertical");
-         horizontal = Input.GetAxis("Horizontal");
+        // _camera.SetActive(true);
+        //if (canmove)
+        //{
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
         //move player based on WASD Input
-        Vector3 movement = transform.forward * vertical + transform.right * horizontal; //changed this line.
+        Vector3 movement = Player.instance.hmdTransform.TransformDirection(new Vector3(horizontal, 0, vertical));
         _charController.Move(movement * Time.deltaTime * _speed);
 
-       }
+        //}
 
 
         //Get Mouse position Input
-        if (!dm.isTalking)
+        /*if (!dm.isTalking)
         {
             float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity; //changed this line.
             float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity; //changed this line.
@@ -85,83 +79,85 @@ public class movement : MonoBehaviour
             _camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
             //Rotate the player based on the X input of the mouse
             transform.Rotate(Vector3.up * mouseX * 3);
+        }*/
+
+
+        moveVector = Vector3.zero;
+
+        //Check if cjharacter is grounded
+        if (_charController.isGrounded == false)
+        {
+            //Add our gravity Vector
+            moveVector += Physics.gravity;
         }
-        
-            
-            moveVector = Vector3.zero;
- 
-         //Check if cjharacter is grounded
-         if (_charController.isGrounded == false)
-         {
-             //Add our gravity Vector
-             moveVector += Physics.gravity;
-         }
- 
-         //Apply our move Vector , remeber to multiply by Time.delta
-         _charController.Move(moveVector * Time.deltaTime);
+
+        //Apply our move Vector , remeber to multiply by Time.delta
+        _charController.Move(moveVector * Time.deltaTime);
 
 
-         
+
         //float horizontal = Input.GetAxis("Horizontal");
-            if(Input.GetKeyDown(KeyCode.C)){
-            if(istpp==false){
-            //_camera=_tppcamera;
-            _fppc.SetActive(false);
-                //_tppc.SetActive(true);
-            istpp=true;
-        }
-        else if(istpp==true){
-            _camera=_fppcamera;
-            _fppc.SetActive(true);
-                //_tppc.SetActive(false);
-            istpp=false;
-        }
-        
-        }
-            
-        if(Input.GetKeyDown(KeyCode.Y)){
-                           
-                        StartCoroutine(waiter());
-        }
-                
-            //      if(n%2==0){
-            //                 iswaving=true;
-            //                 n++;
-            //     }
-            //     else{
-            //                     iswaving=false;
-            //                     n++;
-            //     }
-             
-        
-        }
-          void FixedUpdate() {
-             // _camera.SetActive(true);
-               if(vertical>0||vertical<0||horizontal>0||horizontal<0){
-                         animator.SetBool("isWalking",true);
-              
-
-
-            }
-            else if(horizontal==0&&vertical==0){
-             animator.SetBool("isWalking",false);
-
-            }
-            else{
-                  animator.SetBool("isWalking",false);
-            }
-            // if(iswaving){
-            //     animator.SetBool("iswaving",true);
-            //     canmove=false;
-            // }
-            // else{
-            //                     animator.SetBool("iswaving",false);
-            //                     canmove=true;
-
-            // }
-
-            
-        
+        /*if(Input.GetKeyDown(KeyCode.C)){
+        if(istpp==false){
+        //_camera=_tppcamera;
+        _fppc.SetActive(false);
+            //_tppc.SetActive(true);
+        istpp=true;
     }
-    
+    else if(istpp==true){
+        _camera=_fppcamera;
+        _fppc.SetActive(true);
+            //_tppc.SetActive(false);
+        istpp=false;
+    }
+
+    }
+
+    if(Input.GetKeyDown(KeyCode.Y)){
+
+                    StartCoroutine(waiter());
+    }
+
+        //      if(n%2==0){
+        //                 iswaving=true;
+        //                 n++;
+        //     }
+        //     else{
+        //                     iswaving=false;
+        //                     n++;
+        //     }
+
+
+    }*/
+    }
+    void FixedUpdate()
+    {
+        // _camera.SetActive(true);
+        /*if(vertical>0||vertical<0||horizontal>0||horizontal<0){
+                  animator.SetBool("isWalking",true);
+
+
+
+     }
+     else if(horizontal==0&&vertical==0){
+      animator.SetBool("isWalking",false);
+
+     }
+     else{
+           animator.SetBool("isWalking",false);
+     }*/
+        // if(iswaving){
+        //     animator.SetBool("iswaving",true);
+        //     canmove=false;
+        // }
+        // else{
+        //                     animator.SetBool("iswaving",false);
+        //                     canmove=true;
+
+        // }
+
+
+
+    }
+
 }
