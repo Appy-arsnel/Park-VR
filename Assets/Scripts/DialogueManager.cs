b5e858ba2ebebb772c8f52204aff5ebc985b1c72
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2022-07-04 09:28:19 +0530

    Commit

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -14,52 +14,46 @@ public class DialogueManager : MonoBehaviour
     float distance;
 
     public GameObject player;
-    public GameObject dialogueUI;
+    public GameObject dialogueUI,arrow;
 
-    public NPCDialogueTriggers dt;
+    public Raycast rc;
 
     public Text npcName;
         
     public Text npcDialogueBox;
 
-    
-
 //npc animations
      private Animator animator;
      private bool is_waving;
 
-    //public BlendBetweenCameras blendBC;
-    
+    public BlendBetweenCameras blendBC;
+    [HideInInspector]
     public int i = 0;
-    
-    public int j = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         dialogueUI.SetActive(false);
         animator= GetComponent<Animator>();
+        arrow.SetActive(false);
+        
     }
 
     void Update()
     {
-        if (dt.isGuide || dt.isFlowerBox)
+        if (rc.isGuide)
         {
             Convo();
         }
-        /*if (dt.isGuide)
-        {
-            Convo();
-        }*/
 
-        /*if (blendBC.timer1 > 0f)
-        {
-            blendBC.timer1 -= Time.deltaTime;
-        }
-        if(blendBC.timer1 < 0f)
-        {
-            blendBC.timer1 = 0f;
-        }*/
+        // if (blendBC.timer1 > 0f)
+        // {
+        //     blendBC.timer1 -= Time.deltaTime;
+        // }
+        // if(blendBC.timer1 < 0f)
+        // {
+        //     blendBC.timer1 = 0f;
+        // }
     }
 
     void StartConversation()
@@ -74,7 +68,7 @@ public class DialogueManager : MonoBehaviour
     {
         isTalking = false;
         dialogueUI.SetActive(false);
-        
+        i = 0;
     }
    IEnumerator waving_waiter()
 {     
@@ -86,88 +80,85 @@ public class DialogueManager : MonoBehaviour
            
 
 }
-    public void Convo()
+    void Convo()
     {
         //trigger dialogue
-        if (dt.isGuide)
+        if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
         {
-            if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
-            {
-                StartConversation();
-                StartCoroutine(waving_waiter());
-
-            }
-            else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
-            {
-                EndDialogue();
-                StartCoroutine(waving_waiter());
-                i = 0;
-            }
-            if (isTalking == true && Input.GetKeyDown(KeyCode.Space) && i == 0)
-            {
-                i=1;
-                npcDialogueBox.text = npc.dialogue[i];
-
-            }else if(isTalking == true && Input.GetKeyDown(KeyCode.Space) && i == 1)
-            {
-                i = 5;
-                npcDialogueBox.text = npc.dialogue[i];
-            }else if (isTalking == true && Input.GetKeyDown(KeyCode.Space) && i == npc.dialogue.Length - 1)
-            {
-                EndDialogue();
-                StartCoroutine(waving_waiter());
-                i = 0;
-            }
-        }
-
-        if (dt.isFlowerBox)
-        {
-            
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (isTalking == true && i < 4)
-                {
-                    i++;
-                    npcDialogueBox.text = npc.dialogue[i];
-                } else if(isTalking == true && i == 4)
-                {
-                    i = 2;
-                    npcDialogueBox.text = npc.dialogue[i];
-                }
-            }
-        }
+            StartConversation();
+          StartCoroutine(waving_waiter());
         
-
-        /*if(blendBC.timer1 == 0f)
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
         {
-            if (blendBC.FPOVCamera)
-            {
+            EndDialogue();
+           StartCoroutine(waving_waiter());
+        }
+
+        // if(blendBC.timer1 == 0f)
+        // {
+        //     if (blendBC.FPOVCamera)
+        //     {
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     if (isTalking == true && i < npc.dialogue.Length - 1)
                     {
+                        
                         if (i == 1)
-                        {
-                            i = 5;
-                            npcDialogueBox.text = npc.dialogue[i];
-                            blendBC.redialog = false;
+                        {   arrow.SetActive(true);
+                            dialogueUI.transform.localPosition= new Vector3(-4.76000023f,0.558000028f,1.74800003f);  
+                            player.transform.position=new Vector3(36.0186005f,-3.7019546f,71.5500031f);
                         }
-                        else
-                        {
-                            i++;
-                            npcDialogueBox.text = npc.dialogue[i];
-                            blendBC.redialog = false;
+                         if (i == 2)
+                        {   arrow.transform.position= new Vector3(37.5480003f,-3.18799996f,71.6719971f);
+                           
                         }
-                    }
-                    else if (isTalking == true && i == npc.dialogue.Length - 1)
+                         if (i == 3)
+                        {   arrow.transform.position= new Vector3(37.5480003f,-3.18799996f,72.4830017f);
+                            
+                        }
+                         if (i == 4)
+                        {
+                              dialogueUI.transform.localPosition= new Vector3(-16.1700001f,0.558000028f,2.00999999f);  
+                             player.transform.position=new Vector3(36.0186005f,-3.7019546f,61.9399986f);
+                        }
+                            ++i;
+                            npcDialogueBox.text = npc.dialogue[i];
+                           
+                        }
+                          else if (isTalking == true && i == npc.dialogue.Length - 1)
                     {
-                        EndDialogue();
-                        blendBC.redialog = false;
+                          arrow.SetActive(false);
+                             EndDialogue();
+                      
                     }
-                }
-            } else
-            {
-                if (blendBC.flowerBox)
+                    }
+                  
+                
+            // } else
+            // {
+            //     if (blendBC.flowerBox)
+            //     {
+            //         if (Input.GetKeyDown(KeyCode.Space))
+            //         {
+            //             if (isTalking == true && i < 4)
+            //             {
+            //                 i++;
+            //                 npcDialogueBox.text = npc.dialogue[i];
+            //                 blendBC.redialog = false;
+
+            //             }
+            //             else if (isTalking == true && i == 4)
+            //             {
+            //                 i = 2;
+            //                 npcDialogueBox.text = npc.dialogue[i];
+            //                 blendBC.redialog = false;
+            //             }
+
+            //         }
+            //     }
+            // }
+                /*if(blendBC.flowerBox)
                 {
                     if (Input.GetKeyDown(KeyCode.Space))
                     {
@@ -177,19 +168,46 @@ public class DialogueManager : MonoBehaviour
                             npcDialogueBox.text = npc.dialogue[i];
                             blendBC.redialog = false;
 
-                        }
-                        else if (isTalking == true && i == 4)
+                        } else if(isTalking == true && i == 4)
                         {
                             i = 2;
                             npcDialogueBox.text = npc.dialogue[i];
                             blendBC.redialog = false;
                         }
-
+                        
+                    }
+                } else
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        
+                        if (isTalking == true && i < npc.dialogue.Length - 1)
+                        {
+                            if(i == 1)
+                            {
+                                i = 5;
+                                npcDialogueBox.text = npc.dialogue[i];
+                                blendBC.redialog = false;
+                            }else
+                            {
+                                i++;
+                                npcDialogueBox.text = npc.dialogue[i];
+                                blendBC.redialog = false;
+                            }
+                            
+                        }
+                        else if (isTalking == true && i == npc.dialogue.Length - 1)
+                        {
+                            EndDialogue();
+                            blendBC.redialog = false;
+                        }
                     }
                 }
-            }            
-        }*/
-
+                
+            }*/
+            
+        //}
+  
     }
      void FixedUpdate()
     {
